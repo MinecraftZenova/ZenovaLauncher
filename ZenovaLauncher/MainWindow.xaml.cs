a2006033047a5f4ccf7703ebcb72d1f995a25332
Andrew Miller <trinitydevelopers4christ@gmail.com>
2020-03-27 12:36:43 -0500

    Add HamburgerMenu sidebar

--- ZenovaLauncher/MainWindow.xaml.cs
+++ ZenovaLauncher/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,8 +21,14 @@ namespace ZenovaLauncher
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow() {
+        public MainWindow()
+        {
             InitializeComponent();
         }
+        private void NavView_ItemInvoked(object sender, HamburgerMenuItemInvokedEventArgs e)
+        {
+            
+        }
     }
+
 }
