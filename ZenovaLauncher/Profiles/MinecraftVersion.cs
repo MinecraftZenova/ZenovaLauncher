899418fa6ba46ab489e356a8eebaec002664dfc2
Andrew Miller <trinitydevelopers4christ@gmail.com>
2020-04-03 17:56:35 -0500

    Begin work on version list

---
+++ ZenovaLauncher/Profiles/MinecraftVersion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenovaLauncher
+{
+    class MinecraftVersion
+    {
+        private string _name;
+        private string _uuid;
+        private bool _isBeta;
+
+        public MinecraftVersion(string name, string uuid, bool isBeta)
+        {
+            _name = name;
+            _uuid = uuid;
+            _isBeta = isBeta;
+        }
+
+        public string Name
+        {
+            get { return _name + (_isBeta ? " (Beta)" : ""); }
+        }
+    }
+}
---
+++ ZenovaLauncher/Profiles/Profile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenovaLauncher
+{
+    class Profile
+    {
+        private MinecraftVersion _version;
+        
+        public Profile(string name, MinecraftVersion version)
+        {
+            ProfileName = name;
+            _version = version;
+        }
+
+        public string ProfileName { get; set; }
+
+        public string VersionName
+        {
+            get { return _version.Name; }
+        }
+    }
+
+    class Profiles : ObservableCollection<Profile>
+    {
+        public Profiles()
+        {
+            Add(new Profile("Latest release", new MinecraftVersion("1.14.30.2", "uuid", false)));
+            Add(new Profile("Latest beta", new MinecraftVersion("1.16.0.55", "uuid", true)));
+        }
+    }
+}
