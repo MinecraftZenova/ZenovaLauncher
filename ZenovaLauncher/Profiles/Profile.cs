2cd7750987b6f238ddd05a2bafb4e23f0907afbc
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2020-04-04 20:42:40 -0500

    Profile tests and improvements

--- ZenovaLauncher/Profiles/Profile.cs
+++ ZenovaLauncher/Profiles/Profile.cs
@@ -31,6 +31,16 @@ namespace ZenovaLauncher
         {
             Add(new Profile("Latest release", new MinecraftVersion("1.14.30.2", "uuid", false)));
             Add(new Profile("Latest beta", new MinecraftVersion("1.16.0.55", "uuid", true)));
+            Add(new Profile("This is a very long profile name that could potentially cause problems", new MinecraftVersion("1.14.30.2", "uuid", false)));
+            Add(new Profile("Profile1", new MinecraftVersion("1.14.30.2", "uuid", false)));
+            Add(new Profile("Profile2", new MinecraftVersion("1.14.30.2", "uuid", false)));
+            Add(new Profile("Profile3", new MinecraftVersion("1.14.30.2", "uuid", false)));
+            Add(new Profile("Profile4", new MinecraftVersion("1.14.30.2", "uuid", false)));
+            Add(new Profile("Profile5", new MinecraftVersion("1.14.30.2", "uuid", false)));
+            Add(new Profile("Profile6", new MinecraftVersion("1.14.30.2", "uuid", false)));
+            Add(new Profile("Profile7", new MinecraftVersion("1.14.30.2", "uuid", false)));
+            Add(new Profile("Profile8", new MinecraftVersion("1.14.30.2", "uuid", false)));
+            Add(new Profile("Profile9", new MinecraftVersion("1.14.30.2", "uuid", false)));
         }
     }
 }
