e3e374050b7edd7aee360379f716d19bcf04bc3e
Andrew Miller <trinitydevelopers4christ@gmail.com>
2020-05-26 10:11:37 -0500

    Fix very long load and add splash screen

--- ZenovaLauncher/App.xaml.cs
+++ ZenovaLauncher/App.xaml.cs
@@ -20,12 +20,17 @@ namespace ZenovaLauncher
 
         private const string AppID = "ZenovaApplicationID";
 
+        public static Stopwatch sw;
+        private static SplashScreen splash;
+
         [STAThread]
         public static void Main()
         {
             if (SingleInstance<App>.InitializeAsFirstInstance(AppID))
             {
                 var application = new App();
+                splash = new SplashScreen("Assets/zenova_splash.png");
+                splash.Show(false);
                 application.InitializeComponent();
                 application.Run();
                 // Allow single instance code to perform cleanup operations
@@ -54,29 +59,45 @@ namespace ZenovaLauncher
 
         public void AppStart(object sender, StartupEventArgs e)
         {
+            sw = Stopwatch.StartNew();
             SetupEnvironment();
             Trace.Listeners.Add(new TextWriterTraceListener(new FileStream(Path.Combine(DataDirectory, "log.txt"), FileMode.Create)));
             Trace.AutoFlush = true;
-            Trace.WriteLine("AppStart");
+            Trace.WriteLine("AppStart " + sw.ElapsedMilliseconds + " ms");
             VersionDownloader.standard = new VersionDownloader();
+            Trace.WriteLine("VersionDownloader.standard " + sw.ElapsedMilliseconds + " ms");
             VersionDownloader.user = new VersionDownloader();
+            Trace.WriteLine("VersionDownloader.user " + sw.ElapsedMilliseconds + " ms");
             VersionManager.instance = new VersionManager(VersionsDirectory);
+            Trace.WriteLine("VersionManager.instance " + sw.ElapsedMilliseconds + " ms");
             ProfileManager.instance = new ProfileManager(DataDirectory);
+            Trace.WriteLine("ProfileManager.instance " + sw.ElapsedMilliseconds + " ms");
             ProfileLauncher.instance = new ProfileLauncher();
+            Trace.WriteLine("ProfileLauncher.instance " + sw.ElapsedMilliseconds + " ms");
             AccountManager.instance = new AccountManager();
+            Trace.WriteLine("AccountManager.instance " + sw.ElapsedMilliseconds + " ms");
             ModManager.instance = new ModManager(ModsDirectory);
+            Trace.WriteLine("ModManager.instance " + sw.ElapsedMilliseconds + " ms");
             Task loadTask = Task.Run(async () =>
             {
                 await AccountManager.instance.AddAccounts();
+                Trace.WriteLine("AccountManager.AddAccounts " + sw.ElapsedMilliseconds + " ms");
                 await VersionManager.instance.LoadMinecraftVersions();
+                Trace.WriteLine("VersionManager.LoadMinecraftVersions " + sw.ElapsedMilliseconds + " ms");
                 ModManager.instance.LoadMods();
+                Trace.WriteLine("ModManager.LoadMods " + sw.ElapsedMilliseconds + " ms");
                 ProfileManager.instance.ImportProfiles();
+                Trace.WriteLine("ProfileManager.ImportProfiles " + sw.ElapsedMilliseconds + " ms");
                 Preferences.LoadPreferences(DataDirectory);
+                Trace.WriteLine("Preferences.LoadPreferences " + sw.ElapsedMilliseconds + " ms");
                 VersionManager.instance.RemoveUnusedVersions();
+                Trace.WriteLine("VersionManager.RemoveUnusedVersions " + sw.ElapsedMilliseconds + " ms");
             });
             loadTask.Wait();
             ReadCommandArgs(Environment.GetCommandLineArgs());
-            Trace.WriteLine("AppStart Finished");
+            Trace.WriteLine("AppStart Finished " + sw.ElapsedMilliseconds + " ms");
+            sw.Stop();
+            splash.Close(TimeSpan.FromSeconds(1));
         }
 
         public void AppExit(object sender, ExitEventArgs e)
@@ -131,7 +152,7 @@ namespace ZenovaLauncher
                 string path = Path.Combine(DataDirectory, _directoryMods);
                 if (!Directory.Exists(path))
                     Directory.CreateDirectory(path);
-                Utils.AddSecurityToDirectory(_dataDirectory);
+                Utils.AddSecurityToDirectory(path);
                 return path;
             }
         }
