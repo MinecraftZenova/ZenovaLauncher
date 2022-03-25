13e53dbb4f58e715349fb96808a6b53327535946
ThePixelGamer <keefjacob@gmail.com>
2022-03-25 05:33:02 -0500

    Cut the fat in ModLoader.cpp
    If this is gonna be used permanently might as well

--- ZenovaLoader/src/ModLoader.cpp
+++ ZenovaLoader/src/ModLoader.cpp
@@ -9,21 +9,22 @@
 #include "Utils/AppUtils.h"
 #include "Utils/ProcessUtils.h"
 
-/* including for archival purposes
+/* including for archival purposes 
+// need to interface with ZenovaLauncher logger once implemented
 #define FMT_HEADER_ONLY
 #include <fmt/format.h>
 #include <fmt/xchar.h>
 
 template<typename... Args>
 void print(const std::string& format, const Args&... args) {
-	std::string s = "[ZenovaLoader] " + fmt::format(format, args...) + "\n";
+	std::string s = "[ZenovaLoader] " + fmt::format(format, args...) + '\n';
 	OutputDebugStringA(s.c_str());
 	std::cout << s;
 }
 
 template<typename... Args>
 void print(const std::wstring& format, const Args&... args) {
-	std::wstring s = L"[ZenovaLoader] " + fmt::format(format, args...) + L"\n";
+	std::wstring s = L"[ZenovaLoader] " + fmt::format(format, args...) + L'\n';
 	OutputDebugStringW(s.c_str());
 	std::wcout << s;
 }
@@ -34,26 +35,7 @@ void print(const Str& format, const Args&... args) {}
 
 namespace ModLoader {
 
-// helper types
-template<typename T>
-class unique_hlocal_ptr {
-	T ptr = NULL;
-
-public:
-	~unique_hlocal_ptr() {
-		if (ptr != NULL)
-			LocalFree((HLOCAL)ptr);
-	}
-
-	operator T&() {
-		return ptr;
-	}
-
-	T* operator&() {
-		return &ptr;
-	}
-};
-
+// helper type
 class unique_handle {
 	HANDLE mHandle = NULL;
 
@@ -69,59 +51,11 @@ public:
 	}
 };
 
-DWORD AdjustGroupPolicy(const wchar_t* wstrFilePath) {
-	PACL pOldDACL;
-	unique_hlocal_ptr<PACL> pNewDACL;
-	unique_hlocal_ptr<PSECURITY_DESCRIPTOR> pSD;
-	PSID pSID;
-	EXPLICIT_ACCESS_W eaAccess;
-	SECURITY_INFORMATION siInfo = DACL_SECURITY_INFORMATION;
-	DWORD dwResult = ERROR_SUCCESS;
-
-	// Get a pointer to the existing DACL (Conditionaly).
-	dwResult = GetNamedSecurityInfoW(wstrFilePath, SE_FILE_OBJECT, siInfo, NULL, NULL, &pOldDACL, NULL, &pSD);
-	if (dwResult != ERROR_SUCCESS) {
-		printf("GetNamedSecurityInfo Error %u\n", dwResult);
-		return dwResult;
-	}
-
-	ConvertStringSidToSidW(L"S-1-15-2-1", &pSID);
-	if (pSID == NULL) {
-		DWORD error = GetLastError();
-		printf("ConvertStringSidToSidW Error %u\n", error);
-		return error;
-	}
-
-	ZeroMemory(&eaAccess, sizeof(EXPLICIT_ACCESS_W));
-	eaAccess.grfAccessPermissions = GENERIC_READ | GENERIC_EXECUTE;
-	eaAccess.grfAccessMode = SET_ACCESS;
-	eaAccess.grfInheritance = SUB_CONTAINERS_AND_OBJECTS_INHERIT;
-	eaAccess.Trustee.TrusteeForm = TRUSTEE_IS_SID;
-	eaAccess.Trustee.TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP;
-	eaAccess.Trustee.ptstrName = (LPWSTR)pSID;
-
-	// Create a new ACL that merges the new ACE into the existing DACL.
-	dwResult = SetEntriesInAclW(1, &eaAccess, pOldDACL, &pNewDACL);
-	if (dwResult != ERROR_SUCCESS) {
-		printf("SetEntriesInAcl Error %u\n", dwResult);
-		return dwResult;
-	}
-
-	// Attach the new ACL as the object's DACL.
-	dwResult = SetNamedSecurityInfoW((LPWSTR)wstrFilePath, SE_FILE_OBJECT, siInfo, NULL, NULL, pNewDACL, NULL);
-	if (dwResult != ERROR_SUCCESS) {
-		printf("SetNamedSecurityInfo Error %u\n", dwResult);
-		return dwResult;
-	}
-
-	return dwResult;
-}
-
 BOOL InjectDLL(DWORD dwProcessId, const std::wstring& dllPath) {
 	/* Open the process with all access */
 	unique_handle hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessId);
 	if (hProc == NULL) {
-		print("Could not open the process (%u) HRESULT: %u", dwProcessId, GetLastError());
+		print("Could not open the process ({}) HRESULT: {}", dwProcessId, GetLastError());
 		return FALSE;
 	}
 
@@ -129,33 +63,32 @@ BOOL InjectDLL(DWORD dwProcessId, const std::wstring& dllPath) {
 	SIZE_T dllPathSize = dllPath.size() * sizeof(wchar_t);
 	LPVOID hRemoteMem = VirtualAllocEx(hProc, NULL, dllPathSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 	if (hRemoteMem == NULL) {
-		print("Could not allocate memory in the process (%u) HRESULT: %u", dwProcessId, GetLastError());
+		print("Could not allocate memory in the process ({}) HRESULT: {}", dwProcessId, GetLastError());
 		return FALSE;
 	}
 
 	/* Write the path to the DLL File in the memory just allocated */
 	if (!WriteProcessMemory(hProc, hRemoteMem, dllPath.data(), dllPathSize, NULL)) {
-		print("Could not write memory in the process (%u) HRESULT: %u", dwProcessId, GetLastError());
+		print("Could not write memory in the process ({}) HRESULT: {}", dwProcessId, GetLastError());
 		return FALSE;
 	}
 
 	/* Find the address of the LoadLibrary API */
 	HMODULE hLocKernel32 = GetModuleHandleW(L"Kernel32");
 	if (hLocKernel32 == NULL) {
-		print("Could not get a handle on Kernel32 in the process (%u) HRESULT: %u", dwProcessId, GetLastError());
+		print("Could not get a handle on Kernel32 in the process ({}) HRESULT: {}", dwProcessId, GetLastError());
 		return FALSE;
 	}
 
 	FARPROC hLocLoadLibrary = GetProcAddress(hLocKernel32, "LoadLibraryW");
 	if (hLocLoadLibrary == NULL) {
-		print("Could not find the locatin of LoadLibraryW in the process (%u) HRESULT: %u", dwProcessId, GetLastError());
+		print("Could not find the locatin of LoadLibraryW in the process ({}) HRESULT: {}", dwProcessId, GetLastError());
 		return FALSE;
 	}
-
 	/* Create a remote thread that invokes LoadLibrary for our DLL */
 	unique_handle hRemoteThread = CreateRemoteThread(hProc, NULL, 0, (LPTHREAD_START_ROUTINE)hLocLoadLibrary, hRemoteMem, 0, NULL);
 	if (hRemoteThread == NULL) {
-		print("Could not create a remote thread in the process (%u) HRESULT: %u", dwProcessId, GetLastError());
+		print("Could not create a remote thread in the process ({}) HRESULT: {}", dwProcessId, GetLastError());
 		return FALSE;
 	}
 
@@ -170,7 +103,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	bool debug = false;
 
 	for(int i = 1; i < __argc; i += 2) {
-		//printf("%s, %s\n", argv[i], argv[i+1]);
+		print("{}, {}", __argv[i], __argv[i+1]);
+
 		std::string arg(__argv[i]);
 		if(arg == "-p") {
 			dwProcessId = atoi(__argv[i + 1]);
@@ -232,7 +166,6 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 				print(L"Path: {}", zenovaPath);
 
 				//Assume the game is suspended and inject ZenovaAPI
-				//ModLoader::AdjustGroupPolicy(zenovaPath.c_str()); //not really needed seeing how ZenovaLauncher adjusts the group policy
 				ModLoader::InjectDLL(dwProcessId, zenovaPath + L"\\ZenovaAPI.dll");
 			}
 		}
